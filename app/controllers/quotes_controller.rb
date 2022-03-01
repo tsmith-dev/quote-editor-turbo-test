6d427a3a6739eca4e38402103cb45471b701cbf8
Travis Smith <travis.smith@mac.com>
2022-03-01 13:21:55 -0600

    use Quote ordered scope

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered
   end
 
   def show
@@ -18,7 +18,10 @@ class QuotesController < ApplicationController
   def create
     @quote = Quote.new(quote_params)
     if @quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream
+      end    
     else
       render :new, status: :unprocessable_entity
     end
@@ -34,7 +37,12 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully deleted."
+
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully deleted." }
+      format.turbo_stream
+    end
+    
   end
   
 
