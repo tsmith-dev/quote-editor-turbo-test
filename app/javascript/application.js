08cf3a54e66ec6d5a2f2d187d6ee0c701291a3c4
Travis Smith <travis.smith@mac.com>
2022-03-06 12:40:23 -0600

    re-enable turbo drive

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,3 @@
 // Entry point for the build script in your package.json
 import "./controllers"
-
-// The two following lines disable Turbo on the whole application
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
-// Disabled below and added 2 above to turn off Turbo
-// import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
