e31fc066aaa4c9b53da615bda4f4d1abfb45ec5d
Travis Smith <travis.smith@mac.com>
2022-03-06 12:33:13 -0600

    flash component and styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
 import "./controllers"
+
+// The two following lines disable Turbo on the whole application
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
+// Disabled below and added 2 above to turn off Turbo
+// import "@hotwired/turbo-rails"
