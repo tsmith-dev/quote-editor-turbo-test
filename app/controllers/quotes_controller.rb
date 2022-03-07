4d89d8edfbedee99f29fae60a762deb6a450ab38
Travis Smith <travis.smith@mac.com>
2022-03-07 09:20:23 -0600

    Quote CRUD flash messages

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -20,7 +20,7 @@ class QuotesController < ApplicationController
     if @quote.save
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
       end    
     else
       render :new, status: :unprocessable_entity
@@ -29,7 +29,10 @@ class QuotesController < ApplicationController
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
+      end  
     else
       render :edit, status: :unprocessable_entity
     end
@@ -40,7 +43,7 @@ class QuotesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to quotes_path, notice: "Quote was successfully deleted." }
-      format.turbo_stream
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully deleted." }
     end
     
   end
