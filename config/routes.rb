c6a8e8e283e8fa1ea2571d9db493086feee73ee7
Travis Smith <travis.smith@mac.com>
2022-03-08 12:46:35 -0600

    LineItemDate CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "pages#home"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, only: [:new, :edit, :create, :update, :destroy]
+  end
 end
